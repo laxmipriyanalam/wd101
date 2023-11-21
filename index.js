bcdda08680ec459adec911faa7f03d80a63adb9e
laxmipriyanalam <151514413+laxmipriyanalam@users.noreply.github.com>
2023-11-21 23:53:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,74 +1,43 @@
-const userForm=document.getElementById('user-form');
-const userDataTable=document.getElementById('user-data');
-const UserDataTableBody=userDataTable.querySelector('tbody');
-const dobInput=document.getElementById('dob');
-const dobError=document.getElementById('dobError');
+document.addEventListener('DOMContentLoaded', function () {
+            const form = document.querySelector('form');
+            const table = document.querySelector('table');
 
-window.addEventListener('load',()=>{updateUserDataTable();});
-userForm.addEventListener('submit',(event)=>{event.preventDefault();
+            form.addEventListener('submit', function (event) {
+                event.preventDefault();
 
-const userData={
-    name:document.getElementById('name').value,
-    email:document.getElementById('email').value,
-    password:document.getElementById('password').value,
-    dob:document.getElementById('dob').value,
-    terms:document.getElementById('terms').checked,
-};
+                // Fetch form values
+                const name = document.getElementById('name').value;
+                const email = document.getElementById('email').value;
+                const password = document.getElementById('password').value;
+                const dob = document.getElementById('dob').value;
+                const terms = document.getElementById('terms').checked;
 
-if(!validateUserData(userData)){
-    const errorMessage=document.createElement('p');
-    errorMessage.textContent='Value must be 09/11/1967 or later';
-    errorMessage.classList.add('error-message');
+                // Calculate age
+                const today = new Date();
+                const birthDate = new Date(dob);
+                let age = today.getFullYear() - birthDate.getFullYear();
 
-    const dateField=document.getElementById('dob');
-    dateField.parentNode.appendChild(errorMessage);
-}else{
-    saveUserData(userData);
-    updateUserDataTable();
-    clearForm();
-}
-});
-function validateUserdata(userdata){
-    const minAge=18;
-    const maxAge=55;
-    const today=new Date();
-    const birthDate=new Date(userdata.dob);
-    const age=today.getFullYear()-birthDate.getFullYear();
-    if(age<minAge||age>maxAge){
-        return false;
-    }
-    return true;
-}
-function saveUserData(userData){
-    const existingUserData=JSON.parse(localStorage.getItem('userList')) || [];
-    existingUserData.push(userData);
-    localStorage.setItem('userList',JSON.stringify(existingUserData));
-}
-function updateUserDataTable(){
-    UserDataTableBody.innerHTML='';
-    const userList=JSON.parse(localStorage.getItem('userList')) || [];
-    userList.forEach(userData=>{
-        const userDataRow=createUserDataTableRow(userData);
-        UserDataTableBody.appendChild(userDataRow);
-    });
-    if (userList.length>0){
-        userDataTable.classList.remove('hidden');
-    }else{
-        userDataTable.classList.add('hidden');
-    }
-}
-function createUserDataTableRow(userData){
-    const row=document.createElement('tr');
-    row.innerHTML=`
-    <td>${userData.name}</td>
-    <td>${userData.email}</td>
-    <td>${userData.password}</td>
-    <td>${userData.dob}</td>
-    <td>${userData.terms ? 'true' : 'false'}</td>
-    `;
-    return row;
-}
-function clearForm(){
-    userForm.reset();
-}
-    
+                // Adjust age if birthday hasn't occurred yet this year
+                if (today.getMonth() < birthDate.getMonth() ||
+                    (today.getMonth() === birthDate.getMonth() && today.getDate() < birthDate.getDate())) {
+                    age--;
+                }
+
+                // Check if age is within the specified range
+                if (age >= 18 && age <= 55) {
+                    // Add a new row to the table
+                    const newRow = table.insertRow(-1);
+                    const cells = [name, email, password, dob, terms ? 'Accepted' : 'Not Accepted'];
+
+                    for (const cellValue of cells) {
+                        const cell = newRow.insertCell();
+                        cell.textContent = cellValue;
+                    }
+
+                    // Clear the form
+                    form.reset();
+                } else {
+                    alert('Age must be between 18 and 55 years.');
+                }
+            });
+        });
